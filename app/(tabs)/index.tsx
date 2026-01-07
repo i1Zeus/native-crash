4925b9aa0c4318240e3a55da27e336032d51f90b
Hussein Najah <husseinnajah123@gmail.com>
2026-01-07 22:37:44 +0300

    Refactor Index component to use Tailwind classes for styling
    - Removed inline styles and replaced them with Tailwind CSS classes for a cleaner and more maintainable layout.
    - Simplified the component structure by eliminating the StyleSheet import and associated styles.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.view}>
+    <View className="flex-1 justify-center items-center">
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
