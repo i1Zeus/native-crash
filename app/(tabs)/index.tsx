fda997c3bf4cc53a76a44313d84b04c42e33557a
Hussein Najah <husseinnajah123@gmail.com>
2026-01-08 16:26:04 +0300

    Implement sign-out functionality and enhance route guarding logic
    - Added `signOut` method to the authentication context for user session management.
    - Updated the `Index` component to include a sign-out button.
    - Improved route guarding in the layout to handle loading state and redirect users appropriately based on authentication status.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,15 @@
+import { useAuth } from "@/lib/auth-context";
 import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function Index() {
+  const { signOut } = useAuth();
   return (
     <View className="flex-1 justify-center items-center">
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button onPress={signOut} icon={"logout"}>
+        Sign Out
+      </Button>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,13 +5,17 @@ import "./globals.css";
 
 function RouteGuard({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  const { user } = useAuth();
+  const { user, isLoadingUser } = useAuth();
   const segments = useSegments();
 
   useEffect(() => {
-    const inAuth = segments[0] === "auth";
-    if (!user && inAuth) router.replace("/auth");
-    else if (user && inAuth) router.replace("/");
+    const inAuthGroup = segments[0] === "auth";
+
+    if (!user && !inAuthGroup && !isLoadingUser) {
+      router.replace("/auth");
+    } else if (user && inAuthGroup && !isLoadingUser) {
+      router.replace("/");
+    }
   }, [user, segments]);
 
   return <>{children}</>;
